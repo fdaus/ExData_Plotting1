18facb4da75cbf833a51ddea445fda5a8dad96d1
Firdaus Afifi <fdaus.isa@gmail.com>
2016-10-19 15:27:13 +0800

    updated code

--- plot4.R
+++ plot4.R
@@ -1,6 +1,21 @@
 #plot 4
 
+url =  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip";
+download.file(url, destfile = "exdata.zip")
+unzip("exdata.zip")
+exdata <- read.csv2(file = "household_power_consumption.txt",stringsAsFactors = FALSE)
 
+#convert time format
+x <- paste(exdata$Date,exdata$Time,sep = ",")
+Date<- strptime(x, "%d/%m/%Y,%H:%M:%S")
+
+#add new date column
+xdata <- cbind(Date,subset(exdata,select = Time:Sub_metering_3))
+
+#select data only from this date
+finalData<- subset(xdata, Date >= "2007-02-01"&Date < "2007-02-03")
+
+#output device png
 png("plot4.png", width = 480,height = 480)
 
 #4th graph
